7f6313ed92eba777c459cb09c6455d58cf9d7bf3
hiyama <itk.hym1029@gmail.com>
2022-12-12 19:42:12 +0900

    edit some files

--- pybricks/sbc/sbc.c
+++ pybricks/sbc/sbc.c
@@ -1,9 +1,3 @@
-// SPDX-License-Identifier: MIT
-/*
- * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
- *                    Graduate School of Information Science, Nagoya Univ., JAPAN
- */
-
 #include <t_syslog.h>
 #include "kernel_cfg.h"
 #include "sbc.h"
