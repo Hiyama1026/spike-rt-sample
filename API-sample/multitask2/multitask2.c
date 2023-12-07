20bd5d68e894de03aed2351b815510da32adf664
hiyama <itk.hym1029@gmail.com>
2023-12-07 10:21:08 +0900

    update multitask2

--- API-sample/multitask2/multitask2.c
+++ API-sample/multitask2/multitask2.c
@@ -13,12 +13,25 @@
 #include <spike/hub/bluetooth.h>
 #include "spike/pup/motor.h"
 
-
+SYSTIM st;
 pbio_error_t ma_err, mb_err;
 pup_motor_t *motor_a;
 
 int motor_direction = CLOCKWISE;
 int motor_speed = 800;
+
+void busy_wait (void)   // Wait for10 ms
+{
+  int32_t start_tim, cur_tim;
+  get_tim(&st);
+  start_tim = (int32_t)st;
+
+  while(cur_tim - start_tim <= 10*1000) {
+    get_tim(&st);
+    cur_tim = (int32_t)st;
+  }
+  return;
+}
 
 void
 ctrl_task(intptr_t exinf)   //3s周期かつmotor_task()より高優先度．
@@ -34,7 +47,7 @@ motor_task(intptr_t exinf)
 {
   while(1) {
     pup_motor_set_speed(motor_a, motor_speed * motor_direction);
-    dly_tsk(10*1000);
+    busy_wait();
   }
 }
 
