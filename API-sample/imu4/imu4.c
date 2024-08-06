25a2ba2afe2c514253f1d6a5b6e08e76cc6e738b
hiyama <itk.hym1029@gmail.com>
2024-08-06 19:18:30 +0900

    modified imu4

--- API-sample/imu4/imu4.c
+++ API-sample/imu4/imu4.c
@@ -6,9 +6,10 @@
 #include "kernel_cfg.h"
 #include "imu4.h"
 #include <spike/hub/imu.h>
-#include "spike/hub/display.h"
+#include "spike/hub/button.h"
 
 float x_angle = 0;
+hub_button_t pressed_ptn, pre_ptn;
 
 void
 ctrl_task(intptr_t exinf)   //10ms周期
@@ -16,8 +17,16 @@ ctrl_task(intptr_t exinf)   //10ms周期
   float hub_velocity[3];
   hub_imu_get_angular_velocity(&hub_velocity[0]);
 
-  // Hub停止時の角速度の値が-1となる（個体差？）ため，角速度1から-1の範囲を0とする
-  if (hub_velocity[2] <= 1 && (int)hub_velocity[2] >= -1){
+  hub_button_is_pressed(&pressed_ptn);
+  if ((pressed_ptn & HUB_BUTTON_CENTER) && !(pre_ptn & HUB_BUTTON_CENTER)){
+    hub_imu_init();
+    x_angle = 0;
+    syslog(LOG_NOTICE, "reset");
+  }
+  pre_ptn = pressed_ptn;
+
+  // Hub停止時の角速度の値が-1となる（個体差？）ため，角速度0から-1の範囲を0とする
+  if (hub_velocity[2] <= 0 && (int)hub_velocity[2] >= -1){
     return;
   }
   
